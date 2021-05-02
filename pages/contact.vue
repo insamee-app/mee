e3c8308f62c5b1dbae98fb5d8aed0f26810a214a
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-02 18:43:18 +0200

    feat(contact): add spaces

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
     <div class="text-center text-lg">
-      <div>Besoin <span class="font-bold">d'aide</span> ?</div>
-      <div>Une <span class="font-bold">suggestion</span> ?</div>
-      <div>Un <span class="font-bold">bogue</span> trouvé ?</div>
-      <div>Un <span class="font-bold">mot</span> gentil ?</div>
+      <div class="grid gap-2 mb-8">
+        <div>Besoin <span class="font-bold">d'aide</span> ?</div>
+        <div>Une <span class="font-bold">suggestion</span> ?</div>
+        <div>Un <span class="font-bold">bogue</span> trouvé ?</div>
+        <div>Un <span class="font-bold">mot</span> gentil ?</div>
+      </div>
       <AppButton>Nous contacter</AppButton>
     </div>
   </section>
