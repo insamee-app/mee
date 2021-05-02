8b198a812d288a2e126065a9e2916995464c07e2
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-02 21:35:40 +0200

    fix(header): align title

--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -1,7 +1,12 @@
 <template>
-  <header class="flex flex-row justify-between p-4 absolute w-full">
-    <div class="flex flex-row"><Insamee class="h-6 w-6 mr-2" />INSAMEE</div>
-    <div class="flex flex-row align-middle">
+  <header
+    class="flex flex-row justify-between align-middle p-4 absolute w-full"
+  >
+    <div class="flex flex-row">
+      <Insamee class="h-8 w-8 mr-2" />
+      <div class="self-center">INSAMEE</div>
+    </div>
+    <div class="flex flex-row">
       <AppButton border class="mr-8" :to="{ name: 'signin' }"
         >S'incrire</AppButton
       >
