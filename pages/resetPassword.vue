f6eede89879619cd9f9ebdb16616bdf4bd117871
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-03 10:00:35 +0200

    feat(resetPassword): create page

---
+++ pages/resetPassword.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <AppInput
+      type="password"
+      name="password"
+      placeholder="*******"
+      class="w-full mb-2"
+    >
+      <template #label> Mot de passe </template>
+    </AppInput>
+    <AppInput
+      type="password"
+      name="password_confirmation"
+      placeholder="*******"
+      class="w-full mb-8"
+    >
+      <template #label> Confirmer le mot de passe </template>
+    </AppInput>
+    <AppButton large class="w-full">Modifier son mot de passe</AppButton>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'minimal',
+}
+</script>
