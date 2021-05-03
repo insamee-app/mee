03a2cb8eb0ba92531b694e67738317d0f9ed246e
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-03 10:18:27 +0200

    feat(sendVerifyEmai): create page

---
+++ pages/sendVerifyEmail.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <AppInput
+      type="email"
+      name="email"
+      placeholder="exemple@insamee.fr"
+      class="w-full mb-8"
+    >
+      <template #label> Adresse électronique </template>
+    </AppInput>
+    <AppButton large class="w-full"
+      >Envoyer le couriel de vérification</AppButton
+    >
+  </section>
+</template>
