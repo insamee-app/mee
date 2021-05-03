823df253464fd7cbd36c60e3e5d71abbf730e970
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-03 09:56:08 +0200

    feat(sendResetPassword): create page

---
+++ pages/sendResetPassword.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <AppInput
+      type="email"
+      name="email"
+      placeholder="exemple@insamee.fr"
+      class="w-full mb-8"
+    >
+      <template #label> Adresse électronique </template>
+    </AppInput>
+    <AppButton large class="w-full"
+      >Envoyer le couriel de réinitialisation</AppButton
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'minimal',
+}
+</script>
