08ae5ddeae6a36896d08a771110870e40d66ce93
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-04 10:54:44 +0200

    feat(modal): add outside click

--- components/molecules/AppModal.vue
+++ components/molecules/AppModal.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="fixed top-0 bottom-0 left-0 right-0 z-30">
-    <div class="relative bg-grey-light opacity-30 w-full h-full"></div>
-    <div class="absolute top-0 bottom-0 left-0 right-0">
-      <div class="w-full h-full flex flex-col justify-center p-4">
+  <div
+    v-if="value"
+    class="fixed top-0 bottom-0 left-0 right-0 z-30 flex flex-col justify-center"
+  >
+    <div
+      class="absolute bg-grey-light opacity-30 w-full h-full"
+      @click="clickOutside"
+    ></div>
+    <div class="relative">
+      <div class="flex flex-col justify-center p-4">
         <slot></slot>
       </div>
     </div>
@@ -12,5 +18,16 @@
 <script>
 export default {
   name: 'AppModal',
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    clickOutside() {
+      this.$emit('input', false)
+    },
+  },
 }
 </script>
--- pages/mee/_id.vue
+++ pages/mee/_id.vue
@@ -4,12 +4,14 @@
     <div class="my-4 flex flex-row justify-end sticky bottom-4">
       <AppButton large @click="dialog = true">Contacter</AppButton>
     </div>
-    <AppModal v-if="dialog">
+    <AppModal v-model="dialog">
       <div class="bg-grey-light rounded p-4">
         <div class="flex flex-row justify-between mb-2">
           <div class="text-xl font-bold">lorem ipsum</div>
           <div @click="dialog = false">
-            <IconDismiss class="h-8 w-8 fill-current text-primary-dark" />
+            <IconDismiss
+              class="h-8 w-8 fill-current text-primary-dark cursor-pointer"
+            />
           </div>
         </div>
         <AppItemLabeled class="mb-4">
