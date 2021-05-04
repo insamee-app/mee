2eb66adaa171b8ca046a660ba95592a5824a4f42
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-04 12:31:18 +0200

    feat(legal-mentions): add legal mentions

---
+++ pages/legal-mentions.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppContainer class="mb-4">
+    <NuxtContent
+      :document="content"
+      class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl"
+    />
+  </AppContainer>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const content = await $content('legal-mentions').fetch()
+
+    return {
+      content,
+    }
+  },
+}
+</script>
