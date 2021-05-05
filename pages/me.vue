0bce79e04150ab998a1c595b72b201854a2695cf
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-05 22:25:39 +0200

    feat(me): create page

---
+++ pages/me.vue
@@ -0,0 +1,33 @@
+<template>
+  <AppContainer
+    ><AppProfil />
+    <section class="mb-4">
+      <h2 class="text-2xl font-bold mb-2">Moyen pour me contacter</h2>
+      <AppContact :links="links" />
+    </section>
+    <div class="flex flex-row justify-end sticky bottom-4 mb-4">
+      <AppButton large>Editer le profil</AppButton>
+    </div>
+    <div class="mb-4">
+      <h2 class="text-2xl font-bold mb-4">Zone de danger</h2>
+      <div class="flex flex-col items-center">
+        <AppButton large class="mb-4">Modifier son mot de passe</AppButton>
+        <AppButton large border>Supprimer son compte</AppButton>
+      </div>
+    </div>
+  </AppContainer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: {
+        messenger: 'url',
+        intagram: 'url',
+        twitter: 'url',
+      },
+    }
+  },
+}
+</script>
