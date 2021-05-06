51a129cf584d5f809ddc67013d791a2f52a32bf5
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-06 08:00:17 +0200

    feat(filters): create select

---
+++ components/atoms/AppSelect.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-col">
+    <label :for="name" class="mb-1"><slot></slot></label>
+    <select
+      :id="name"
+      :name="name"
+      class="py-1 px-2 border border-primary-dark rounded bg-grey-light"
+    >
+      <option
+        v-for="item in selectItems"
+        :key="item.id"
+        :value="item.id"
+        class="hover:bg-primary-dark"
+      >
+        {{ item.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSelect',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    selectItems() {
+      const item = { id: '', name: 'Veuillez choisir une valeur' }
+      const items = this.items.slice()
+      items.unshift(item)
+      return items
+    },
+  },
+}
+</script>
--- pages/mee/index.vue
+++ pages/mee/index.vue
@@ -1,5 +1,28 @@
 <template>
   <section class="pt-16 px-4">
+    <div class="flex flex-row justify-end">
+      <AppButton large @click="filterDialog = true">Filtrer</AppButton>
+      <AppModal v-model="filterDialog">
+        <div class="bg-grey-light rounded p-4">
+          <div class="flex flex-row justify-between mb-2">
+            <div class="text-xl font-bold">Filtres</div>
+            <div @click="filterDialog = false">
+              <IconDismiss
+                class="h-8 w-8 fill-current text-primary-dark cursor-pointer"
+              />
+            </div>
+          </div>
+          <div class="mb-4">
+            <AppSelect name="focusInterest" :items="focusInterestItems"
+              >Centre d'interêt</AppSelect
+            >
+          </div>
+          <div class="flex flex-row justify-end">
+            <AppButton>Valider</AppButton>
+          </div>
+        </div>
+      </AppModal>
+    </div>
     <AppCard
       v-for="value in 3"
       :key="value"
@@ -36,3 +59,31 @@
     </AppCard>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterDialog: false,
+      focusInterestItems: [
+        {
+          id: 1,
+          name: 'talent',
+        },
+        {
+          id: 1,
+          name: 'talent',
+        },
+        {
+          id: 1,
+          name: 'talent',
+        },
+        {
+          id: 1,
+          name: 'talent',
+        },
+      ],
+    }
+  },
+}
+</script>
