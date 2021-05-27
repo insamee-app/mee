33906cdcb0a91881f325e272b44252fa6d2b10b8
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-05-27 15:27:15 +0200

    fix(chip): update text size

--- components/atoms/AppChip.vue
+++ components/atoms/AppChip.vue
@@ -1,5 +1,7 @@
 <template>
-  <li class="flex-shrink-0 bg-grey-base text-white rounded-full px-2 py-1">
+  <li
+    class="flex-shrink-0 bg-grey-base text-white text-sm rounded-full px-2 py-1"
+  >
     <slot></slot>
   </li>
 </template>
