a4769d63911de1e5e683056a528b734812703ff5
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-11 11:20:33 +0200

    fix(components): add new components

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,17 +3,17 @@
     <template v-if="error.statusCode === 404">
       <h1 class="text-primary-dark font-bold text-4xl">404</h1>
       <div class="mt-8">Il n'y a pas de Mee par ici !</div>
-      <NuxtLink :to="{ name: 'login' }" class="text-primary-dark mt-4 block"
-        >Trouver de nouveaux Mee ?</NuxtLink
+      <InsameeAppLink :link="{ name: 'login' }" class="mt-4 block"
+        >Trouver de nouveaux Mee ?</InsameeAppLink
       >
     </template>
     <template v-else>
       <h1>Il semble qu'une erreur soit survenue !</h1>
-      <AppButton large class="w-full mt-8" :to="{ name: 'index' }"
-        >Accueil</AppButton
+      <InsameeAppButton large class="w-full mt-8" :to="{ name: 'index' }"
+        >Accueil</InsameeAppButton
       >
-      <AppButton border class="w-full mt-8" @click="show = !show"
-        >En savoir plus</AppButton
+      <InsameeAppButton border class="w-full mt-8" @click="show = !show"
+        >En savoir plus</InsameeAppButton
       >
       <pre v-if="show" class="mt-8 overflow-x-auto text-left">{{ error }}</pre>
     </template>
