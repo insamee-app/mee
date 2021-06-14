a7bc586e667c4885fe6f9d93a1f03a30660c6741
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-14 17:35:34 +0200

    fix(reload): issue on v-if

--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -1,18 +1,22 @@
 <template>
   <InsameeHeader :icon-link="iconLink" @open="open">
     <template #actions>
-      <InsameeAppButton v-if="loggedIn()" :to="{ name: 'mee' }">
-        Trouver des mee
-      </InsameeAppButton>
-      <InsameeAppButton v-else border :to="{ name: 'signin' }">
-        S'incrire
-      </InsameeAppButton>
+      <div>
+        <InsameeAppButton v-if="loggedIn()" :to="{ name: 'mee' }">
+          Trouver des mee
+        </InsameeAppButton>
+        <InsameeAppButton v-else border :to="{ name: 'signin' }">
+          S'incrire
+        </InsameeAppButton>
+      </div>
     </template>
   </InsameeHeader>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+
+console.log('the header')
 
 export default {
   name: 'TheHeader',
