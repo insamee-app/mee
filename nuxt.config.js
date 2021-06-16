940e4f5b6b3e6e194073ba303beeb9fd2c2e7120
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-16 19:33:43 +0200

    feat(screen): add programmatic breakpoints

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,8 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    // https://www.npmjs.com/package/vue-screen
+    'vue-screen/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
