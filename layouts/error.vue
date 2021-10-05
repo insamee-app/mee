9b181a4bca67035a2b635578bb3852d34b55fc8c
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-05 11:49:56 +0200

    chore: move to yarn

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="text-center">
     <template v-if="error.statusCode === 404">
-      <Insamee404 :error="error" :to="{ name: 'mee' }">
+      <InsameeError404 :error="error" :to="{ name: 'mee' }">
         Trouver un mee ?
-      </Insamee404>
+      </InsameeError404>
     </template>
     <template v-else>
       <InsameeErrorGlobal :error="error" :to="{ name: 'index' }">
